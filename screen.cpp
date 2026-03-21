ad91edd7cef4a5b9809a64331505cd87d06e6f19
steve <jscrane@gmail.com>
2026-03-21 19:57:31 +0000

    cleanup state handling

--- screen.cpp
+++ screen.cpp
@@ -11,34 +11,22 @@
 
 void Screen::on_mode_change() {
 
-	bool text = _switches.is_text(), mixed = _switches.is_mixed(), hgr = _switches.is_hires();
-	bool top_text = text && !hgr, btm_text = text || mixed;
-	uint8_t state = (text << 4) | (hgr << 3) | (_switches.is_page2() << 2) | (btm_text << 1) | top_text;
+	uint8_t state = (_switches.is_text() << 3) | (_switches.is_mixed() << 2) |
+			(_switches.is_page2() << 1) | (_switches.is_hires() << 0);
 
 	if (state == _state) return;
 
-	DBG_DSP("text=%d mixed=%d hgr=%d", text, mixed, hgr);
+	bool text = (state >> 3) & 1, mixed = (state >> 2) & 1, hgr = state & 1;
+	bool top_hgr = !text && hgr, btm_text = text || mixed;
 
-	if (text) {
-		lores.set_top_active(true);
-		lores.set_btm_active(true);
-		hires.set_top_active(false);
-		hires.set_btm_active(false);
-	} else if (mixed) {
-		lores.set_top_active(!hgr);
-		lores.set_btm_active(true);
-		hires.set_top_active(hgr);
-		hires.set_btm_active(false);
-	} else {
-		lores.set_top_active(!hgr);
-		lores.set_btm_active(!hgr);
-		hires.set_top_active(hgr);
-		hires.set_btm_active(hgr);
-	}
+	hires.set_top_active(top_hgr);
+	lores.set_top_active(!top_hgr);
+	hires.set_btm_active(!btm_text && hgr);
+	lores.set_btm_active(btm_text);
 
 	uint8_t diff = state ^ _state;
-	if ((diff & 16) || (diff & 8) || (diff & 4) || (diff & 1)) {
-		if (top_text)
+	if (diff & 0b1011) {	// skip if only "mixed" changed
+		if (text)
 			lores.redraw(0, SPLIT_LINE, true);
 		else if (hgr)
 			hires.redraw(0, SPLIT_LINE);
@@ -46,15 +34,12 @@ void Screen::on_mode_change() {
 			lores.redraw(0, SPLIT_LINE, false);
 	}
 
-	if ((diff & 16) || (diff & 8) || (diff & 4) || (diff & 2)) {
-		if (btm_text)
-			lores.redraw(SPLIT_LINE, SCREEN_LINES, true);
-		else if (hgr)
-			hires.redraw(SPLIT_LINE, SCREEN_LINES);
-		else
-			lores.redraw(SPLIT_LINE, SCREEN_LINES, false);
-	}
+	if (btm_text)
+		lores.redraw(SPLIT_LINE, SCREEN_LINES, true);
+	else if (hgr)
+		hires.redraw(SPLIT_LINE, SCREEN_LINES);
+	else
+		lores.redraw(SPLIT_LINE, SCREEN_LINES, false);
 
 	_state = state;
 }
-
